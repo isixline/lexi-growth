e53c021c4c93563b6dc67bdfb6bd8148f18016f1
isixline <isixline@qq.com>
2024-02-18 18:14:20 +0800

    feat: statistic words from text by frequency

---
+++ tests/counters/test_frequency_counter.py
@@ -0,0 +1,25 @@
+from lexi_growth.counters.frequency_counter import count_words_from_file, handle_counte_words_by_frequency
+from tests.utils.file_compare_util import compare_files
+import os
+
+def test_count_words_from_file():
+    # given 
+    file_path = "tests/test_data/counte_words.txt"
+
+    # when
+    word_count = count_words_from_file(file_path)
+
+    # then
+    assert word_count == {'hello': 3, 'python': 1, 'world': 2}
+
+def test_handle_counte_words_by_frequency():
+    # given
+    file_path = "tests/test_data/counte_words.txt"
+    expected_file_path = "tests/test_data/counte_words_by_frequency_expected.csv"
+
+    # when
+    output_file_path = handle_counte_words_by_frequency(file_path)
+
+    # then
+    compare_files(output_file_path, expected_file_path)
+    os.remove(output_file_path)
