af24faa1424a1a67528eff72815e980974cfb403
isixline <isixline@qq.com>
2024-02-19 19:36:51 +0800

    feat: word English-English translation

--- lexi_growth/handlers/file_handler.py
+++ lexi_growth/handlers/file_handler.py
@@ -1,6 +1,7 @@
 from lexi_growth.extracters.ass_text_extracter import handle_extract_ass_text
 from lexi_growth.counters.frequency_counter import handle_counte_words_by_frequency
 from lexi_growth.filters.known_word_filter import handle_filter_known_word
+from lexi_growth.translators.english_english_translator import handle_translate_english_english
 
 def handle_file_type(file_path):
     if file_path.lower().endswith('.ass'):
@@ -12,7 +13,8 @@ def handle_file(file_path):
     processing_functions = [
         handle_file_type,
         handle_counte_words_by_frequency,
-        handle_filter_known_word
+        handle_filter_known_word,
+        handle_translate_english_english
     ]
 
     for func in processing_functions:
---
+++ lexi_growth/translators/english_english_translator.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import requests
+
+def get_word_english_definition(word):
+    print(f"Getting definition for {word}")
+    url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            definitions = [definition['definition'] for entry in data for meaning in entry.get('meanings', []) for definition in meaning.get('definitions', [])]
+            return '; '.join(definitions)
+        else:
+            return 'Not found'
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 'Error'
+    
+def handle_translate_english_english(file_path):
+    df = pd.read_csv(file_path)
+    
+    df['english_definition'] = df['word'].apply(get_word_english_definition)
+    
+    output_path = file_path.replace('.csv', '_translated.csv')
+    df.to_csv(output_path, index=False)
+    
+    return output_path
