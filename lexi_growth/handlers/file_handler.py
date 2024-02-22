eceb151ad77c926242393257f2fccc5f41bdcffc
isixline <isixline@qq.com>
2024-02-22 18:08:31 +0800

    feat: export result file

--- lexi_growth/handlers/file_handler.py
+++ lexi_growth/handlers/file_handler.py
@@ -2,6 +2,14 @@ from lexi_growth.extracters.extract_distributor import handle_extract_file_text
 from lexi_growth.counters.frequency_counter import handle_counte_words_by_frequency
 from lexi_growth.filters.known_word_filter import handle_filter_known_word
 from lexi_growth.translators.english_english_translator import handle_translate_english_english
+from lexi_growth.utils.file_util import copy_file
+import os
+
+def export_result_file(file_path):
+    workspace_path = os.getenv('WORKSPACE_PATH')
+    result_file_path = f"{workspace_path}/result.csv" 
+    copy_file(file_path, result_file_path)
+    return result_file_path
 
 def handle_file(file_path):
     processing_functions = [
@@ -14,6 +22,5 @@ def handle_file(file_path):
     for func in processing_functions:
         file_path = func(file_path)
 
-    
-
-    return file_path
+    result_file_path = export_result_file(file_path)
+    return result_file_path
--- lexi_growth/utils/file_util.py
+++ lexi_growth/utils/file_util.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def converte_to_workspace_process_file_path(output_file_name, output_file_type):
     workspace_path = os.getenv('WORKSPACE_PATH')
@@ -6,3 +7,7 @@ def converte_to_workspace_process_file_path(output_file_name, output_file_type):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return f"{directory}/{output_file_name}.{output_file_type}"
+
+def copy_file(source_file_path, target_file_path):
+    shutil.copyfile(source_file_path, target_file_path)
+    return target_file_path
