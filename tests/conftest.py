aa64f831ae69999eea333566345f7e7351b846e8
isixline <isixline@qq.com>
2024-02-23 18:14:27 +0800

    fix: test load_dotenv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from dotenv import load_dotenv
+import os
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_environment():
     # setup environment
     print("Setting up environment")
-    load_dotenv('tests/.env.test')
+    os.environ.clear()
+    load_dotenv(dotenv_path="tests/.env.test")
     yield
     # clean up environment
     print("Cleaning up environment")
--- tests/utils/test_file_util.py
+++ tests/utils/test_file_util.py
@@ -1,4 +1,5 @@
 from lexi_growth.utils.file_util import converte_to_workspace_process_file_path
+import os
 
 def test_converte_to_workspace_process_file_path():
     # given
@@ -9,4 +10,5 @@ def test_converte_to_workspace_process_file_path():
     file_path = converte_to_workspace_process_file_path(output_file_name, output_file_type)
 
     # then
+    print("WORKSPACE_PATH", os.getenv('WORKSPACE_PATH'))
     assert file_path == "tests/test_data/workspace/handled/output.csv"
