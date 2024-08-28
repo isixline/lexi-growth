03615700bb7907e35e193a8492df8694848b33d4
isixline <isixline@qq.com>
2024-08-28 16:29:19 +0800

    feat: match searchWords as word

--- lexi-front/src/pages/HighlightText.js
+++ lexi-front/src/pages/HighlightText.js
@@ -3,14 +3,14 @@ import Highlighter from "react-highlight-words";
 
 const HighlightText = ({ text, words }) => {
   const lines = text.split('\n');
+  const searchWords = words.map(wordObj => `\\b${wordObj.word}\\b`);
 
   const highlightedLines = lines.map((line, index) => (
     <p>
       <Highlighter
         key={index}
-        searchWords={words.map(wordObj => wordObj.word)}
-        autoEscape={true}
         textToHighlight={line}
+        searchWords={searchWords}
         caseSensitive={false}
       />
     </p>
